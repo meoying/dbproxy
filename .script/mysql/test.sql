af084be2c8185d289331781480dbb1b4a2e28171
最高 <yezhenpeng@yishouapp.com>
2024-05-26 17:17:29 +0800

    测试数据

--- .script/mysql/test.sql
+++ .script/mysql/test.sql
@@ -67,13 +67,19 @@ CREATE TABLE `test_file_path_type` (
 INSERT INTO `test_int_type` (`id`, `type_tinyint`, `type_smallint`, `type_mediumint`, `type_int`, `type_integer`, `type_bigint`) VALUES (1, 1, 2, 3, 4, 5, 6);
 INSERT INTO `test_int_type` (`id`, `type_tinyint`, `type_smallint`, `type_mediumint`, `type_int`, `type_integer`, `type_bigint`) VALUES (2, 127, 32767, 8388607, 2147483647, 2147483647, 9223372036854775807);
 INSERT INTO `test_int_type` (`id`, `type_tinyint`, `type_smallint`, `type_mediumint`, `type_int`, `type_integer`, `type_bigint`) VALUES (3, -128, -32768, -8388608, -2147483648, -2147483648, -9223372036854775808);
+INSERT INTO `test_int_type` (`id`, `type_tinyint`, `type_smallint`, `type_mediumint`, `type_int`, `type_integer`, `type_bigint`) VALUES (4, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `test_float_type` (`id`, `type_float`, `type_double`, `type_decimal`, `type_numeric`, `type_real`) VALUES (1, 66.66000, 999.99900, 33.33, 123456.78, 12345.6789);
+INSERT INTO `test_float_type` (`id`, `type_float`, `type_double`, `type_decimal`, `type_numeric`, `type_real`) VALUES (2, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `test_string_type` (`id`, `type_char`, `type_varchar`, `type_tinytext`, `type_text`, `type_mediumtext`, `type_longtext`, `type_enum`, `type_set`, `type_binary`, `type_varbinary`, `type_json`, `type_bit`) VALUES (1, '一', '二', '三', '四', '五', '六', 'small', 'b,c', 0x61626300000000000000, 0x616263646566, '{\"age\": 25, \"name\": \"Tom\", \"address\": {\"city\": \"New York\", \"zipcode\": \"10001\"}}', b'0010101010');
+INSERT INTO `test_string_type` (`id`, `type_char`, `type_varchar`, `type_tinytext`, `type_text`, `type_mediumtext`, `type_longtext`, `type_enum`, `type_set`, `type_binary`, `type_varbinary`, `type_json`, `type_bit`) VALUES (2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `test_date_type` (`id`, `type_date`, `type_datetime`, `type_timestamp`, `type_time`, `type_year`) VALUES (1, '2024-05-25', '2024-05-25 23:51:00', '2024-05-25 23:51:05', '23:51:08', 2024);
+INSERT INTO `test_date_type` (`id`, `type_date`, `type_datetime`, `type_timestamp`, `type_time`, `type_year`) VALUES (2, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `test_geography_type` (`id`, `type_geometry`, `type_geomcollection`, `type_linestring`, `type_multilinestring`, `type_point`, `type_multipoint`, `type_polygon`, `type_multipolygon`) VALUES (1, ST_GeomFromText('LINESTRING(0 0, 1 1, 2 2)'), ST_GeomFromText('GEOMETRYCOLLECTION(POINT(1 1), LINESTRING(0 0, 1 1, 2 2))'), ST_GeomFromText('LINESTRING(0 0, 1 1, 2 2)'), ST_GeomFromText('MULTILINESTRING((0 0, 1 1, 2 2), (2 2, 3 3, 4 4))'), ST_GeomFromText('POINT(40.7128 -74.006)'), ST_GeomFromText('MULTIPOINT(40.7128 -74.006, 34.0522 -118.2437)'), ST_GeomFromText('POLYGON((0 0, 0 10, 10 10, 10 0, 0 0))'), ST_GeomFromText('MULTIPOLYGON(((0 0, 0 10, 10 10, 10 0, 0 0)), ((20 20, 20 30, 30 30, 30 20, 20 20)))'));
+INSERT INTO `test_geography_type` (`id`, `type_geometry`, `type_geomcollection`, `type_linestring`, `type_multilinestring`, `type_point`, `type_multipoint`, `type_polygon`, `type_multipolygon`) VALUES (2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `test_file_path_type` (`id`, `type_tinyblob`, `type_mediumblob`, `type_blob`, `type_longblob`) VALUES (1, 0x01020304FFFFFFFF0000000CAACB0000, 0x01020304FFFFFFFF0000000CAACB0000, 0x01020304FFFFFFFF0000000CAACB0000, 0x01020304FFFFFFFF0000000CAACB0000);
+INSERT INTO `test_file_path_type` (`id`, `type_tinyblob`, `type_mediumblob`, `type_blob`, `type_longblob`) VALUES (2, NULL, NULL, NULL, NULL);
