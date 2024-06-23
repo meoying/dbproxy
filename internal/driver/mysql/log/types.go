3abb46273035a9912ce40adef4071f0f8bf4f1c1
longyue0521 <longyueli0521@gmail.com>
2024-06-23 21:20:31 +0800

    去掉logger接口中不必要的方法,重构NewConnector并添加Example测试

--- internal/driver/mysql/log/types.go
+++ internal/driver/mysql/log/types.go
@@ -1,29 +1,14 @@
 package log
 
 import (
-	"context"
 	"database/sql/driver"
 	"log/slog"
 )
 
 //go:generate mockgen -source=./types.go -destination=mocks/logger.mock.go -package=logmocks -typed logger
 type logger interface {
-	Debug(msg string, args ...any)
 	Info(msg string, args ...any)
-	Warn(msg string, args ...any)
 	Error(msg string, args ...any)
-
-	DebugContext(ctx context.Context, msg string, args ...any)
-	InfoContext(ctx context.Context, msg string, args ...any)
-	WarnContext(ctx context.Context, msg string, args ...any)
-	ErrorContext(ctx context.Context, msg string, args ...any)
-
-	Enabled(ctx context.Context, level slog.Level) bool
-
-	With(args ...any) *slog.Logger
-	WithGroup(name string) *slog.Logger
-
-	Handler() slog.Handler
 }
 
 type ConnectorOptions struct {
@@ -39,12 +24,11 @@ func WithLogger(l *slog.Logger) Option {
 }
 
 func NewConnector(d driver.Driver, dsn string, opts ...Option) (driver.Connector, error) {
-	options := &ConnectorOptions{}
+	options := &ConnectorOptions{
+		l: slog.Default(),
+	}
 	for _, opt := range opts {
 		opt(options)
-	}
-	if options.l == nil {
-		options.l = slog.Default()
 	}
 	return newDriver(d, options.l).OpenConnector(dsn)
 }
--- internal/driver/mysql/log/types_test.go
+++ internal/driver/mysql/log/types_test.go
@@ -1,10 +1,15 @@
 package log
 
 import (
+	"bytes"
+	"database/sql"
+	"fmt"
+	"log"
 	"log/slog"
 	"os"
 	"testing"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/meoying/dbproxy/internal/driver/mysql/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -12,7 +17,6 @@ import (
 )
 
 func TestNewConnector(t *testing.T) {
-
 	t.Run("默认log", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
@@ -26,7 +30,6 @@ func TestNewConnector(t *testing.T) {
 		require.NoError(t, err)
 		assert.NotZero(t, conn)
 	})
-
 	t.Run("自定义log", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
@@ -41,4 +44,20 @@ func TestNewConnector(t *testing.T) {
 		require.NoError(t, err)
 		assert.NotZero(t, conn)
 	})
+}
+
+func ExampleNewConnector() {
+	dsn := "root:root@tcp(example.com:3306)/?charset=utf8mb4&parseTime=True&loc=Local"
+	driver := &mysql.MySQLDriver{}
+
+	customLogger := slog.New(slog.NewJSONHandler(&bytes.Buffer{}, nil))
+	connector, err := NewConnector(driver, dsn, WithLogger(customLogger))
+	if err != nil {
+		log.Fatalf("创建连接器失败: %v", err)
+	}
+	db := sql.OpenDB(connector)
+	fmt.Println("使用自定义log创建连接器并获取*sql.DB对象:", db != nil)
+
+	// Output:
+	// 使用自定义log创建连接器并获取*sql.DB对象: true
 }
